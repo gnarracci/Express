f0383de008338313dcf8115d162144c70458dcb2
Gianni Narracci <gnarracci@gmail.com>
2018-10-08 15:38:52 -0400

    Cambios completos y pruebas realizadas

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
 
-app.get('/', (req, res) => {
-    res.end('Hello World');
-});
+// Requiriendo Rutas
+const routes = require('./routes');
+const routesApi = require('./routes-api.js');
+
+// Settings
+app.set('appName', 'Mi Primer Server');
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+// Middlewares
+app.use(morgan('combined'));
+
+// Routes
+app.use(routes);
+app.use('/api', routesApi);
 
 app.listen(3000, () => {
     console.log('Servidor respondiendo en el puerto 3000');
+    console.log('Nombre de la App', app.get('appName'));
 });
